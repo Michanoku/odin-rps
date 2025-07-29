6a0c9eaee998044d9c0e7ff5668c0711b9960da8
Michanoku <michael.werker@gmail.com>
2025-07-29 07:39:52 +0000

    Add random number calculation

--- script.js
+++ script.js
@@ -3,8 +3,10 @@
 // Write a function that returns the computer choice
 function getComputerChoice() {
     // Create a random number between 0 and 1
-    // Modify that number so you can get it between 1.0 and 9.9
-    // If the number is below or equal to 3.3, choose rock
-    // If the number is below or equal to 6.6, choose paper
+    const randomNumber = Math.random();
+    // Modify that number so you can get it between 1 and 100
+    const result = randomNumber * Math.floor(randomNumber * 100) + 1;
+    // If the number is below or equal to 33, choose rock
+    // If the number is below or equal to 66, choose paper
     // For any other result, use scissors
 }
