1d61349e692be70ed1ae728e12ffd57341fd41e1
Michanoku <michael.werker@gmail.com>
2025-07-29 07:41:17 +0000

    Add return values for computer choice

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ function getComputerChoice() {
     // Modify that number so you can get it between 1 and 100
     const result = randomNumber * Math.floor(randomNumber * 100) + 1;
     // If the number is below or equal to 33, choose rock
+    if (result <= 33) {
+        return "Rock!"
     // If the number is below or equal to 66, choose paper
+    } else if (result <= 66) {
+        return "Paper!"
     // For any other result, use scissors
+    } else {
+        return "Scissors!"
+    } 
 }
