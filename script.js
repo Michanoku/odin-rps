9dc37571d714cf25faa24a56e88fbecdee9a2937
Michanoku <michael.werker@gmail.com>
2025-07-29 07:36:51 +0000

    Add function and pseudo code

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // JavaScript File for Odin Rock Paper Scissors
+
+// Write a function that returns the computer choice
+function getComputerChoice() {
+    // Create a random number between 0 and 1
+    // Modify that number so you can get it between 1.0 and 9.9
+    // If the number is below or equal to 3.3, choose rock
+    // If the number is below or equal to 6.6, choose paper
+    // For any other result, use scissors
+}
