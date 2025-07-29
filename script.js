a379697d2017256ae306cf31fa8d101a6ed983b6
Michanoku <michael.werker@gmail.com>
2025-07-29 08:05:14 +0000

    Add code for the winning decision

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ function getComputerChoice() {
     const result = randomNumber * Math.floor(randomNumber * 100) + 1;
     // If the number is below or equal to 33, choose rock
     if (result <= 33) {
-        return "rock"
+        return "rock";
     // If the number is below or equal to 66, choose paper
     } else if (result <= 66) {
-        return "paper"
+        return "paper";
     // For any other result, use scissors
     } else {
-        return "scissors"
+        return "scissors";
     } 
 }
 
@@ -36,16 +36,27 @@ function playRound(computerChoice, humanChoice) {
     // Initialize a result variable
     let result;
     // If the player choice and computer choice is equal
+    if (computerChoice === humanChoice) {
         // Save draw text to result
+        result = "Draw!";
         // No one gets points
-    // If there is no draw
+    // Else, no draw
+    } else {
         // If the player plays rock, and the computer scissors, or the player plays paper and the computer rock, or the player plays scissors and the computer paper:
+        if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
             // Save player wins text to result
+            result = "You win!";
             // Give the player points
+            humanScore++;
         // Else
+        } else {
             // Save computer wins text to result
+            result = "You lose!";
             // Give the computer points
+            computerScore++;
+        }
 
+    }
     // If the player won:
         // Capitalize the first letter of the players choice
         // Return a concatenated string of the result and the reason
